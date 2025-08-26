583766d2acdf30f45005db64473fe52b9fccc843
smstraughan <smstraughan@gmail.com>
2025-08-26 16:43:06 -0600

    week 4 notes

---
+++ week-4/script.js
@@ -0,0 +1,24 @@
+console.log("Open Code Notes")
+
+//single line notes
+
+/* multi line notes
+
+Question 2: Addition Operation
+
+You have been given two variables, num1 and num2, each with predefined values as test cases. 
+Your task is to calculate the sum of these variables and display the result using the console.log() */
+
+var num1 = 10
+var num2 = 2
+
+//add the two variables and display using consol.log
+
+console.log(num1 + num2)
+
+ var x = 25;
+  var y = 3;
+  var z = "Z = " + (x*2);
+
+  console.log(z)
+
